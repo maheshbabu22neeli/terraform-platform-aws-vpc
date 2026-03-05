0d02e8e1c33713ed5a5babaeff43693e23c091fe
Mahesh Babu Neeli <maheshbabu22.neeli@gmail.com>
2026-03-05 17:14:27 +1100

    Terraform VPC: Out Subnet Id's

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,15 @@ output "vpc_id" {
 output "default_vpc_id" {
   value = data.aws_vpc.default.id
 }
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
